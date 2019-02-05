f94e8034df2f1b4f34d895bd23fecdf9f910f6cd
Bogus Franczak <bfranczak@gmail.com>
2019-02-05 17:34:59 +0100

    Add card name change funtionality

--- js/Board.js
+++ js/Board.js
@@ -1,36 +1,57 @@
 var board = {
     name: 'Kanban Board',
-    addColumn: function(column) {
+    addColumn: function (column) {
         this.element.appendChild(column.element);
         initSortable(column.id);
     },
     element: document.querySelector('#board .column-container')
 }
 
-document.querySelector('#board .create-column').addEventListener('click', function(){
+document.querySelector('#board .create-column').addEventListener('click', function () {
     var name = prompt('Enter column name');
     var data = new FormData();
 
-    data.append('name',name);
+    data.append('name', name);
 
     fetch(baseUrl + '/column', {
         method: 'POST',
         headers: myHeaders,
         body: data,
     })
-    .then(function(resp) {
-        return resp.json();
-    })
-    .then(function(resp) {
-        var column = new Column(resp.id, name);
-        board.addColumn(column);
-    })
+        .then(function (resp) {
+            return resp.json();
+        })
+        .then(function (resp) {
+            var column = new Column(resp.id, name);
+            board.addColumn(column);
+        })
 })
 
 function initSortable(id) {
     var el = document.getElementById(id);
     var sortable = Sortable.create(el, {
         group: 'kanban',
-        sort: true
+        sort: true,
+        handle: '.card',
+        onAdd: function (evt) {
+            if (evt.from.id !== evt.to.id) {
+                var myHeaders = {
+                    'X-Client-Id': 3607,
+                    'X-Auth-Token': '436acdcb35d10a21433857565d2eb47f',
+                    'Content-Type': 'application/json'
+                }
+                var cardID = evt.item.id;
+                var colId = evt.to.id;
+                var name = evt.item.querySelector('.card-description').innerText;
+                var body = JSON.stringify({ 'name': name, 'bootcamp_kanban_id': parseInt(colId) });
+                fetch(baseUrl + '/card/' + cardID, { method: 'PUT',headers: myHeaders, body: body })
+                    .then(function (resp) {
+                        return resp.json();
+                    })
+            }
+            console.log('from',evt.from.id);
+            console.log('to', evt.to.id);
+            console.log(evt.item.id);
+        }
     })
 }
--- js/Card.js
+++ js/Card.js
@@ -3,7 +3,8 @@ function Card(id, name, columnId) {
     this.name = name || 'No name given';
     this.id = id;
     this.columnId = columnId;
-    this.element = generateTemplate('card-template', { description: this.name }, 'li');
+    this.element = generateTemplate('card-template', { description: this.name}, 'li');
+    this.element.id = this.id;
     this.element.querySelector('.card').addEventListener('click', function (event) {
         event.stopPropagation();
         if (event.target.classList.contains('btn-delete')) {
@@ -28,19 +29,24 @@ Card.prototype = {
     changeName: function () {
         var self = this;
         var newName = prompt('Please, enter new text...');
-        var data = new FormData();
-        data.append('name', newName);
-        data.append('bootcamp_kanban_column_id', self.columnId);
+        //var data = new FormData();
+        var myHeaders = {
+            'X-Client-Id': 3607,
+            'X-Auth-Token': '436acdcb35d10a21433857565d2eb47f',
+            'Content-Type': 'application/json'
+        }
+        // data.append('name', newName);
+        // data.append('bootcamp_kanban_column_id', self.columnId);
         fetch(baseUrl + '/card/' + self.id, {
             method: 'PUT',
-            body: data,
+            body: JSON.stringify({'name': newName, 'bootcamp_kanban_id': self.columnId}),
             headers: myHeaders
         })
-        .then(function(resp){
-            resp.json();
-        })
-        .then(function(resp) {
-            self.element.querySelector('.card-description').innerText = newName;
-        })
+            .then(function (resp) {
+                resp.json();
+            })
+            .then(function (resp) {
+                self.element.querySelector('.card-description').innerText = newName;
+            })
     }
 }
